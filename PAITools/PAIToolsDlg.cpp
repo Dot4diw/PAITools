3c3316757b334af9487fd8975fe105353dcfb065
Dot4diw <73592345+Dot4diw@users.noreply.github.com>
2023-09-09 09:38:18 +0800

    Update PAIToolsDlg.cpp

--- PAITools/PAIToolsDlg.cpp
+++ PAITools/PAIToolsDlg.cpp
@@ -6,6 +6,7 @@
 #include "framework.h"
 #include "PAITools.h"
 #include "PAIToolsDlg.h"
+#include "converter.h"
 #include "afxdialogex.h"
 #include <Magick++.h> 
 #include <iostream> 
@@ -177,28 +178,6 @@ BOOL CPAIToolsDlg::OnInitDialog()
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
-
-
-BOOL CONVERTER_FUNCTION(std::string INFILE_ARG, std::string OUTPUTFILE_ARG, int QUAL_ARG, std::string DPI_ARG);
-BOOL CONVERTER_FUNCTION(std::string INFILE_ARG, std::string OUTPUTFILE_ARG, int QUAL_ARG, std::string DPI_ARG)
-{
-	try {
-		Magick::InitializeMagick(".");
-		Magick::Image MYIMAGE;
-		MYIMAGE.quality(QUAL_ARG);
-		MYIMAGE.resolutionUnits(Magick::PixelsPerInchResolution);
-		MYIMAGE.density(DPI_ARG);
-		MYIMAGE.read(INFILE_ARG);
-		MYIMAGE.write(OUTPUTFILE_ARG);
-	}
-	catch (Magick::Exception &error_)
-	{
-		std::cout << "Caught exception: " << error_.what() << std::endl;
-		return FALSE;
-	}
-	return TRUE;
-}
-
 
 void CPAIToolsDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
